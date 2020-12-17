e6f0276ec52a00da650a4f86b2306e978db186a2
Benjamin Cheng <bencheng200@gmail.com>
2020-12-17 06:49:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,4 @@
-#
-#class CreateCostumeStores < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 #def change
 #  create_table :costume_stores do |t|
 #    t.string :name
@@ -13,4 +12,4 @@
 #    t.timestamps null: false
 #  end
 #end
-#end
+end
